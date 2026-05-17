889bb5b8cfa05015c452fb4bf0f8b24332af72bb
BrunofCarvalhoo <bcf2@cesar.school>
2026-05-17 15:36:11 -0300

    alguns graficos

---
+++ src/graficos.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def distribuicao_graus(arquivo_grau, arquivo_saida):
+        
+    df_grau = pd.read_csv(arquivo_grau)
+    plt.figure(figsize=(10, 6))
+    faixas = [0, 5, 10, 15, 20, 25, 30]
+    df_grau['grau'].hist(bins=faixas, edgecolor='black', color='#4682B4')
+    plt.title('Distribuição dos Graus dos Aeroportos', fontsize=14, pad=15)
+    plt.xlabel('Graus', fontsize=12)
+    plt.ylabel('Quantidade de Aeroportos', fontsize=12)
+    plt.xticks(faixas)
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    plt.grid(axis='x', visible=False) 
+    plt.savefig(arquivo_saida, bbox_inches='tight')
+    plt.close()
+    
+def graus_por_aeroporto(arquivo_grau, arquivo_saida):
+    df_grau = pd.read_csv(arquivo_grau)
+    df_grau_por_aeroporto = df_grau.sort_values(by='grau', ascending=False)
+    plt.figure(figsize=(14, 6))
+    plt.bar(df_grau_por_aeroporto['aeroporto'], df_grau_por_aeroporto['grau'], 
+            color='#55A868', edgecolor='black') 
+    
+    plt.title('Ranking de grau por Aeroporto', fontsize=14, pad=15)
+    plt.xlabel('Aeroportos (IATA)', fontsize=12)
+    plt.ylabel('Quantidade de Graus', fontsize=12)
+    
+    plt.xticks(rotation=45)
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    plt.savefig(arquivo_saida, bbox_inches='tight')
+    plt.close()
+    
+
+def main():
+    arquivo_grau = 'out/graus.csv'
+    arquivo_saida_distribuicao_graus = 'out/distribuicao_graus.png'
+    arquivo_saida_ranking_graus_aeroportos = 'out/ranking_graus_aeroportos.png' 
+
+    distribuicao_graus(arquivo_grau, arquivo_saida_distribuicao_graus)
+    graus_por_aeroporto(arquivo_grau, arquivo_saida_ranking_graus_aeroportos)
+
+if __name__ == "__main__":
+    main()
